09bbb9e587d179757ff60c4bcf2135d4b08bed3c
gregoryb79 <gregoryb79@gmail.com>
2025-03-11 08:12:33 +0200

    base

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const greet = "Gregory";
+console.log(`Hello ${greet}`);
+let num = 5;
+num = num + 5;
+console.log(`num is: ${num}`);
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const greet = "Gregory";
+console.log(`Hello ${greet}`);
+
+let num = 5;
+
+num = num + 5;
+console.log(`num is: ${num}`);
